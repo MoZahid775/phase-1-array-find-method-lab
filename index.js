4adfa3dd55d977b42748e691161b743a62bd018b
Muhammad Zahid <muhammadali775@gmail.com>
2021-04-29 00:55:09 -0400

    commit tree 6222d5965b2cf71f08ed3508f439677ef004f297

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+
+
+
+
+function superbowlWin(array) {
+   
+       const results = array.find(record => record.result === "W" )
+        
+       
+     
+         return !!results ? results.year : undefined;
+      
+    }
+
